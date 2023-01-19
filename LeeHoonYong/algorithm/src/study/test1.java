85f11aedef9a716155fcc60a6ca59d5998665896
이훈용 <wjaj12@naver.com>
2023-01-19 15:19:02 +0900

    delete test file

--- LeeHoonYong/algorithm/src/study/test1.java
+++
@@ -1,9 +0,0 @@
-package study;
-
-public class test1 {
-
-	public static void main(String[] args) {
-		System.out.println("test");
-	}
-
-}
--- LeeHoonYong/algorithm/src/study/test2.java
+++
@@ -1,9 +0,0 @@
-package study;
-
-public class test2 {
-
-	public static void main(String[] args) {
-		System.out.println("test2");
-	}
-
-}
