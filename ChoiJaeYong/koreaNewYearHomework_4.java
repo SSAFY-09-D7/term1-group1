cc5d3f62cbc947c6d4021581dbd898af79095970
최재용 <johnchoi213.jc@gmail.com>
2023-01-27 11:13:51 +0900

    Update koreaNewYearHomework_4.java

--- ChoiJaeYong/koreaNewYearHomework_4.java
+++ ChoiJaeYong/koreaNewYearHomework_4.java
@@ -1,6 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class koreaNewYearHomework_2 {
+public class koreaNewYearHomework_4 {
 	static int[] direction = {0, 3, 5, 6};
 	
 	public static void main(String[] args) {
@@ -11,30 +12,58 @@ public class koreaNewYearHomework_2 {
 		for(int tc = 1; tc <= t; tc++) {
 			int n = sc.nextInt(), runners = sc.nextInt();
 			int[][] visited = new int[n][n];
-			int ans = 0;
+			int ans = runners;
 			
 			for(int i = 1; i <= runners; i++) {
 				int x = sc.nextInt(), y = sc.nextInt(), d = sc.nextInt();
-				
-				if (ans > 0) continue;
-				
-				if (d == 1) {//하
+
+				if (d == 1) {//상
 					for(int k = 0; k < 4; k++) {
-						if (!(x + direction[k] < n)) break;
-						if (visited[x + direction[k]][y] == 0) visited[x + direction[k]][y] = 1;
-						else {
-							ans = i;
+						if (!(x - direction[k] >= 0)) {
+							ans--;
 							break;
 						}
+						if(visited[x - direction[k]][y] == 1) {
+							ans--;
+							break;
+						}
+						if(k == 3) visited[x - direction[k]][y] = 1;
+					}
+				}else if(d == 2) {//하
+					for(int k = 0; k < 4; k++) {
+						if (!(x + direction[k] < n)) {
+							ans--;
+							break;
+						}
+						if (visited[x + direction[k]][y] == 1) {
+							ans--;
+							break;
+						}
+						if (k == 3) visited[x + direction[k]][y] = 1;
+					}
+				}else if(d == 3) {//좌
+					for(int k = 0; k < 4; k++) {
+						if (!(y - direction[k] >= 0)) {
+							ans--;
+							break;
+						}
+						if (visited[x][y - direction[k]] == 1) {
+							ans--;
+							break;
+						}
+						if (k == 3) visited[x][y - direction[k]] = 1;
 					}
 				}else {//우
 					for(int k = 0; k < 4; k++) {
-						if (!(y + direction[k] < n)) break;
-						if (visited[x][y + direction[k]] == 0) visited[x][y + direction[k]] = 1;
-						else {
-							ans = i;
+						if (!(y + direction[k] < n)) {
+							ans--;
 							break;
 						}
+						if (visited[x][y + direction[k]] == 1) {
+							ans--;
+							break;
+						}
+						if (k == 3) visited[x][y + direction[k]] = 1;
 					}
 				}
 			}
