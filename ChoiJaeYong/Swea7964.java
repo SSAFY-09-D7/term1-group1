1b9a158ad4e1c3c766c7b7daa74ce995f7779137
최재용 <johnchoi213.jc@gmail.com>
2023-02-05 02:59:34 +0900

    Update Swea7964.java

--- ChoiJaeYong/Swea7964.java
+++ ChoiJaeYong/Swea7964.java
@@ -1,46 +1,60 @@
-import java.util.*;
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Swea7964 {
-	public static void main(String[] args) throws IOException {
+	static int[] dx = {-1, 1, 0, 0, -1, 1, -1, 1};
+	static int[] dy = {0, 0, -1, 1, -1, 1, 1, -1};
+	static int N, M;
+	static int[][] arr;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuffer sb = new StringBuffer();
 		
-		int t = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 		
-		for(int tc = 1; tc <= t; tc++) {
+		for(int tc = 1; tc <= T; tc++) {
 			sb.append("#"+tc+" ");
 			st = new StringTokenizer(br.readLine());
-			int n = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken());
-			int[] arr = new int[n + 2];
-			st = new StringTokenizer(br.readLine());
-			int ans = 0;
+			N = Integer.parseInt(st.nextToken());M = Integer.parseInt(st.nextToken());
+			arr = new int[N][N];
+			arr[N / 2][N / 2] = arr[N / 2 - 1][N / 2 - 1] = 2;
+			arr[N / 2 - 1][N / 2] = arr[N / 2][N / 2 - 1] = 1;
 			
-			arr[0] = arr[n + 1] = 1;
-			for(int i = 1; i <= n; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
+			for(int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int ty = Integer.parseInt(st.nextToken()) - 1, tx = Integer.parseInt(st.nextToken()) - 1, color = Integer.parseInt(st.nextToken());
+
+				arr[tx][ty] = color;
+
+				for(int k = 0; k < 8; k++) {
+					int x = tx + dx[k], y = ty + dy[k];
+					
+					while(!(x < 0 || x >= N || y < 0 || y >= N || arr[x][y] == 0)) {
+						if (arr[x][y] == color) {
+							for(int space = 1; space < Math.max(Math.abs(tx - x), Math.abs(ty - y)); space++) {
+								arr[tx + dx[k] * space][ty + dy[k] * space] = color;
+							}
+							break;
+						}else {
+							x += dx[k]; y += dy[k];
+						}
+					}
+				}
 			}
 			
-			int idx = 0;
+			int black = 0, white = 0;
 			
-			while(idx + d < n + 1) {
-				int last = -1;
-				
-				for(int i = idx + 1; i <= idx + d; i++) {
-					if (arr[i] == 1) last = i;
-				}
-				
-				if (last == -1) {
-					arr[idx + d] = 1;
-					ans++;
-					idx += d;
-				}else {
-					idx = last;
+			for(int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					if(arr[i][j] == 1) black++;
+					else if (arr[i][j] == 2) white++;
 				}
 			}
-			sb.append(ans+"\n");
+			sb.append(black+" "+white+"\n");
 		}
+		
 		System.out.println(sb);
 	}
 }
