917a64f6b61bc1fdc21c5a690343666f27877b62
choijehyuk <cjh970324@naver.com>
2023-02-08 00:09:38 +0900

    [백준 15651](N과M_3) Solution Num=21

---
+++ ChoiJeHyuk/BAEKJOON/BOJ15651.java
@@ -0,0 +1,43 @@
+package boj15651;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+				
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s);
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] arr = new int[n];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+		recursive(arr, new int[m], 0);
+		System.out.println(sb);
+	}
+
+	private static void recursive(int[] arr, int[] sel, int k) {
+		// basis part
+		if (k == sel.length) {
+			for (int i = 0; i < sel.length; i++) {
+				sb.append(sel[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			sel[k] = arr[i];
+			recursive(arr, sel, k + 1);
+		}
+	}
+
+}
